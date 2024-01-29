5f6f9d46f52f5c4c83dd1f46faeb5a8eb2a141e8
leiocopio <107382632+leiocopio@users.noreply.github.com>
2024-01-30 01:03:00 +0800

    30 Jan, 2023
    1. Removed nav.js and migrate it to App.js
    2. Added search page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
+import "./App.css";
 
-import './App.css';
-import Navbar from './components/nav';
-import Footer from './components/footer';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { BsMoon } from "react-icons/bs";
+
+//Components and Pages
+import Search from "./pages/search";
+import "./components/css/nav.css";
+import SignIn from "./components/signIn";
+import Footer from "./components/footer";
 
 function App() {
   return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
     <>
-    <Navbar />
+      <nav className="h-[3.875rem] w-full px-[1.25rem] font-bold border-b-2 border-azure-500">
+        {/* Brand Name */}
+        <div className="text-2xl">GabAI</div>
 
-    <div className=' bg-red-500 h-screen w-screen'>
+        {/* Search Bar */}
+        <div className="flex flex-row items-center gap-x-5">
+          {/* Login Btn */}
+          <SignIn />
 
-    {/* 
-    The content lol
-    */}
-    
-    </div>
+          {/* Search Icon */}
+          <Link to="/search">
+            <FaSearch className="text-2xl" />
+          </Link>
 
-<Footer />
+          {/* Toggle night mode */}
+          <BsMoon className="text-2xl" />
+        </div>
+      </nav>
+
+      {/* Main Content */}
+      <div className="flex flex-col items-center justify-center h-screen">
+      </div>
+      <Footer />
     </>
   );
 }
---
+++ src/pages/search.js
@@ -0,0 +1,22 @@
+import React from'react';
+
+
+
+function Search() {
+
+    return (
+    <>
+        <div className='flex flex-col items-center justify-center'>
+            <input type="text" placeholder='Search' className='w-[25rem] border-2 border-black rounded-xl p-2' />
+            
+
+
+        </div>
+
+
+    </>
+    )
+
+}
+
+export default Search;
