e61913973141f2aef3a1315cdfa06aef08cbf731
Ibeneser15 <128778968+Ibeneser15@users.noreply.github.com>
2024-02-03 00:09:55 +0800

    Sec 1 and 2

--- src/components/sect2.js
+++ src/components/sect2.js
@@ -1,9 +1,24 @@
+import sec2 from "../components/img/sec2.png";
+
 function sect2() {
-    return (
-        <div>
-            <h1></h1>
-        </div>
-    )
+  return (
+    <div className="flex flex-row justify-around items-center mx-[25rem]">
+      <div className="grid items-center">
+        <img src={sec2} alt="law " className=" w-[20rem]" />
+      </div>
+      <div className="">
+        <h1 className="text-6xl font-bold mb-4">
+          About <span className="text-azure">GabAi</span>
+        </h1>
+        <p className="text-justify  w-[20rem]">
+          Welcome to GabAi, your go-to web application for comprehensive legal
+          guides against workplace discrimination in the Philippines. Empower
+          yourself with valuable insights into local laws, employee rights, and
+          practical strategies for fostering a fair and inclusive workplace.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default sect2;
