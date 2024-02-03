c70ea9ff4c1c3a453d2a0ed5048c16755c7c85ad
Nicole Del Carmen <131800769+Nicoledelcarmen@users.noreply.github.com>
2024-02-03 14:26:59 +0800

    Update sect4.js

--- src/components/sect4.js
+++ src/components/sect4.js
@@ -1,9 +1,21 @@
-function sect4(){
-    return (
-        <div>
-            <p>Sect 4</p>
-        </div>
-    )
-}
+import React from 'react';
 
-export default sect4;
+const CallToAction = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold mb-4">Let's stop <span className='text-azure'>discrimination!</span></h1>
+        <p className="mb-6">
+          Creating a safe space for everyone, where they can get the help they need against workplace discrimination.
+          <br />
+          GabAi helps you and the many others to shout their voices and be heard!
+        </p>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Join now!
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CallToAction;
