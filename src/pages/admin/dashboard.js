087e3330fbe5e42061abe5bd84c8e6f0786addb4
leiocopio <107382632+leiocopio@users.noreply.github.com>
2024-02-08 00:12:25 +0800

    Update dashboard.js

--- src/pages/admin/dashboard.js
+++ src/pages/admin/dashboard.js
@@ -1,17 +1,31 @@
 import React from "react";
+import { FaUsers } from "react-icons/fa6";
+import { LiaBalanceScaleSolid } from "react-icons/lia";
 
+import { VscFeedback } from "react-icons/vsc";
 function Sidebar() {
   return (
     <>
       <div className="w-[20rem] bg-gray-300 h-screen p-5">
-        <div>
-          <h1 className="text-bold text-2xl">Admin Dashboard</h1>
+        <div className="my-5">
+          <h1 className="font-bold text-2xl">Admin Dashboard</h1>
           <br />
-          <div className="my-5"></div>
+          <div className="my-[10rem]"></div>
           <nav className="flex flex-col gap-10 list-none items-start text-black">
-            <li>Users</li>
-            <li>Cases</li>
-            <li>Feedbacks</li>
+            <li className="flex flex-row gap-2 text-2xl">
+              <FaUsers />
+              Users
+            </li>
+
+            <li className="flex flex-row gap-2 text-2xl">
+              <LiaBalanceScaleSolid />
+              Cases
+            </li>
+
+            <li className="flex flex-row gap-2 text-2xl">
+              <VscFeedback />
+              Feedbacks
+            </li>
           </nav>
         </div>
       </div>
@@ -26,6 +40,8 @@ function AdminDashboard() {
         <div>
           <Sidebar />
         </div>
+
+        <div>{/* Contents */}</div>
       </div>
     </>
   );
