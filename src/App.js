4c7c9c7b797b9c0f4dca5826e64d8b3ac425563b
Ibeneser15 <128778968+Ibeneser15@users.noreply.github.com>
2024-02-08 00:15:20 +0800

    Revert "Sec 1 and 2"
    This reverts commit 3544c9aba65fcb21c0d736d2d738c26fc3658ef5.

--- src/App.js
+++ src/App.js
@@ -15,42 +15,48 @@ import LandingPage from "./pages/landingPage";
 
 function App() {
   return (
+
     <>
       <head>
         <title>GabAI</title>
       </head>
 
-      <Router>
-        {/* Navigation Bar  */}
-        <nav className="h-[3.875rem] w-full px-[1.25rem] font-bold border-b-2 border-azure-500">
-          {/* Brand Name */}
-          <Link to="/">
-            <div className="text-2xl">GabAI</div>
+
+    <Router>
+
+
+      {/* Navigation Bar  */}
+            <nav className="h-[3.875rem] w-full px-[1.25rem] font-bold border-b-2 border-azure-500">
+        {/* Brand Name */}
+        <Link to="/">
+    <div className="text-2xl">GabAI</div>
+        </Link>
+        
+
+        {/* Search Bar */}
+        <div className="flex flex-row items-center gap-x-5">
+
+          {/* Login Btn */}
+          <SignIn />
+
+          {/* Search Icon */}
+          <Link to="/search">
+            <FaSearch className="text-2xl" />
           </Link>
 
-          {/* Search Bar */}
-          <div className="flex flex-row items-center gap-x-5">
-            {/* Login Btn */}
-            <SignIn />
+          {/* Toggle night mode */}
+          <BsMoon className="text-2xl" />
+        </div>
+      </nav>
 
-            {/* Search Icon */}
-            <Link to="/search">
-              <FaSearch className="text-2xl" />
-            </Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/searchResults" element={<Results />} />
+      </Routes>
+    </Router>
 
-            {/* Toggle night mode */}
-            <BsMoon className="text-2xl" />
-          </div>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/searchResults" element={<Results />} />
-        </Routes>
-      </Router>
-
-      <Footer></Footer>
+    <Footer></Footer>
     </>
   );
 }
@@ -58,12 +64,15 @@ function App() {
 function Home() {
   return (
     <>
+
       {/* Main Content */}
-      <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-col h-screen">
+
         {/* Landing Page here!!! */}
 
         <LandingPage></LandingPage>
       </div>
+
     </>
   );
 }
